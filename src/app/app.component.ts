687b703267dffd233c6efd79ddaa7d1af4b3a481
Jérôme BALIEUX <jerome.balieux@axiocode.com>
2019-04-09 10:32:58 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mon-activite-un';
+
+  posts = [
+    { 
+      title: 'Mon premier post',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt provident quisquam asperiores voluptates et, iure quaerat sapiente accusamus vel architecto, aliquid cumque error sint fugit esse tenetur rem totam aut.',
+      loveIts: 1,
+      created_at: '2016-01-12T13:30:46.358+05:00'
+    },
+    { 
+      title: 'Mon deuxième post',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt provident quisquam asperiores voluptates et, iure quaerat sapiente accusamus vel architecto, aliquid cumque error sint fugit esse tenetur rem totam aut.',
+      loveIts: -1,
+      created_at: '2016-01-12T13:30:46.358+05:00'
+    },
+    { 
+      title: 'Mon troisième post',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt provident quisquam asperiores voluptates et, iure quaerat sapiente accusamus vel architecto, aliquid cumque error sint fugit esse tenetur rem totam aut.',
+      loveIts: 0,
+      created_at: '2016-01-12T13:30:46.358+05:00'
+    }
+  ]
 }
